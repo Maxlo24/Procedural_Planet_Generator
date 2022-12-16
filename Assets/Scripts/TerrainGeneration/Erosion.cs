8d5aae2033ef50bc3625485fec88227f97d12643
julienchz <78453441+julienchz@users.noreply.github.com>
2022-12-16 23:04:27 +0100

    Add erosion script

---
+++ Assets/Scripts/TerrainGeneration/Erosion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Erosion : MonoBehaviour
+{
+    //[field: SerializeField] public int iterationNumber { get; private set; }
+    //[field: SerializeField] public int  { get; private set; }
+
+    //public void Erode(float[,] heightMap)
+    //{
+    //    // Generate random water droplet
+    //}
+
+}
